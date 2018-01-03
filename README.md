d26ccc13609a3f112370304a75a1bdec532ca119
Thomas Boerger <thomas@webhippie.de>
2018-01-03 13:18:37 +0100

    Added some more basic structure

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-# homebrew-gomematic
+# Gomematic: Homebrew
 
 [![Build Status](http://github.dronehippie.de/api/badges/gomematic/homebrew-gomematic/status.svg)](http://github.dronehippie.de/gomematic/homebrew-gomematic)
 [![Stories in Ready](https://badge.waffle.io/gomematic/gomematic-api.svg?label=ready&title=Ready)](http://waffle.io/gomematic/gomematic-api)
 [![Join the Matrix chat at https://matrix.to/#/#gomematic:matrix.org](https://img.shields.io/badge/matrix-%23gomematic%3Amatrix.org-7bc9a4.svg)](https://matrix.to/#/#gomematic:matrix.org)
 [![Codacy Badge](https://api.codacy.com/project/badge/Grade/1c9e07db702546e4a9199eb6b961f20f)](https://www.codacy.com/app/gomematic/homebrew-gomematic?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=gomematic/homebrew-gomematic&amp;utm_campaign=Badge_Grade)
 
+
+**This project is under heavy development, it's not in a working state yet!**
 
 Homebrew repository to install Gomematic on macOS.
 
@@ -16,9 +18,26 @@ brew tap gomematic/gomematic
 ```
 
 
+## Install
+
+### gomematic-cli
+
+```bash
+brew install gomematic-cli
+gomematic-cli -h
+```
+
+
+## Development
+
+```
+rake test
+```
+
+
 ## Security
 
-If you find a security issue please contact thomas@webhippie.de first.
+If you find a security issue please contact gomematic@webhippie.de first.
 
 
 ## Contributing
---
+++ gomematic-cli.rb
@@ -0,0 +1,57 @@
+require "formula"
+require "language/go"
+
+class GomematicCli < Formula
+  desc "Lightweight and powerful Homematic - CLI"
+  homepage "https://github.com/gomematic/gomematic-cli"
+
+  stable do
+    url "https://dl.webhippie.de/gomematic/cli/0.1.0/gomematic-cli-0.1.0-darwin-10.6-amd64"
+    sha256 `curl -Ls https://dl.webhippie.de/gomematic/cli/0.1.0/gomematic-cli-0.1.0-darwin-10.6-amd64.sha256`.split(" ").first
+    version "0.1.0"
+  end
+
+  devel do
+    url "https://dl.webhippie.de/gomematic/cli/master/gomematic-cli-master-darwin-10.6-amd64"
+    sha256 `curl -Ls https://dl.webhippie.de/gomematic/cli/master/gomematic-cli-master-darwin-10.6-amd64.sha256`.split(" ").first
+    version "master"
+  end
+
+  head do
+    url "https://github.com/gomematic/gomematic-cli.git", :branch => "master"
+    depends_on "go" => :build
+  end
+
+  test do
+    system "#{bin}/gomematic-cli", "--version"
+  end
+
+  def install
+    case
+    when build.head?
+      ENV["GOPATH"] = buildpath
+      ENV["GOHOME"] = buildpath
+      ENV["CGO_ENABLED"] = 0
+      ENV["TAGS"] = ""
+
+      ENV.prepend_create_path "PATH", buildpath/"bin"
+
+      currentpath = buildpath/"src/github.com/gomematic/gomematic-cli"
+      currentpath.install Dir["*"]
+      Language::Go.stage_deps resources, buildpath/"src"
+
+      cd currentpath do
+        system "make", "test", "build"
+
+        bin.install "gomematic-cli"
+        # bash_completion.install "contrib/bash-completion/_gomematic-cli"
+        # zsh_completion.install "contrib/zsh-completion/_gomematic-cli"
+        prefix.install_metafiles
+      end
+    when build.devel?
+      bin.install "#{buildpath}/gomematic-cli-master-darwin-10.6-amd64" => "gomematic-cli"
+    else
+      bin.install "#{buildpath}/gomematic-cli-0.1.0-darwin-10.6-amd64" => "gomematic-cli"
+    end
+  end
+end
