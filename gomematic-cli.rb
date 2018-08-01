3f5e12a47cd3f1ed28023e016bfd8e856fab562a
Thomas Boerger <thomas@webhippie.de>
2018-08-01 22:13:19 +0200

    Fallback if checksum file do not exist

--- gomematic-cli.rb
+++ gomematic-cli.rb
@@ -13,13 +13,21 @@ class GomematicCli < Formula
 
   stable do
     url "https://dl.webhippie.de/gomematic/cli/0.1.0/gomematic-cli-0.1.0-darwin-10.6-amd64"
-    sha256 open("https://dl.webhippie.de/gomematic/cli/0.1.0/gomematic-cli-0.1.0-darwin-10.6-amd64.sha256").read.split(" ").first
+    sha256 begin
+      open("https://dl.webhippie.de/gomematic/cli/0.1.0/gomematic-cli-0.1.0-darwin-10.6-amd64.sha256").read.split(" ").first
+    rescue
+      nil
+    end
     version "0.1.0"
   end
 
   devel do
     url "https://dl.webhippie.de/gomematic/cli/master/gomematic-cli-master-darwin-10.6-amd64"
-    sha256 open("https://dl.webhippie.de/gomematic/cli/master/gomematic-cli-master-darwin-10.6-amd64.sha256").read.split(" ").first
+    sha256 begin
+      open("https://dl.webhippie.de/gomematic/cli/master/gomematic-cli-master-darwin-10.6-amd64.sha256").read.split(" ").first
+    rescue
+      nil
+    end
     version "master"
   end
 
